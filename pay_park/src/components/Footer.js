ee9649424fe678ea7f425d973a2f04f58757cf22
wanguilindsey <wanguilindsey@gmail.com>
2024-10-10 15:28:54 +0300

    footer

---
+++ pay_park/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+      <footer className="footer">
+        <p className="footer-text">
+          &copy; 2024 ParkPay - Simplifying Parking Payments in Nakuru
+        </p>
+      </footer>
+    );
+};
+  
+export default Footer;
