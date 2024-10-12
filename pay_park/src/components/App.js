960deb2c51ba625331e085da897d98c4501e45e6
wanguilindsey <wanguilindsey@gmail.com>
2024-10-13 00:32:42 +0300

    payment confirmation

--- pay_park/src/components/App.js
+++ pay_park/src/components/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import Form from './Form';
+import PaymentConfirmation from './PaymentConfirmation';
 
 const App = () => {
-    return (
+  return (
+    <Router>
       <div className="App">
         <Header />
-        
+
         <div className="container">
-          <Form />
+        <Routes>
+          <Route path="/" element={<Form />} />
+          <Route path="/payment-confirmation" element={<PaymentConfirmation />} />
+        </Routes>
         </div>
-        
+
         <Footer />
       </div>
-    );
-  }
+    </Router>
+  );
+}
   
-  export default App;
+export default App;
